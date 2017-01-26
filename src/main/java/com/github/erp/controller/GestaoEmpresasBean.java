2fd8453ff4d4e83a09f293ed2d1c74ef14ff2a51
Adriano <adrianofalcao@gmail.com>
2017-01-26 16:19:45 -0300

    Começando com primefaces

---
+++ src/main/java/com/github/erp/controller/GestaoEmpresasBean.java
@@ -0,0 +1,36 @@
+package com.github.erp.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.github.erp.model.Empresa;
+import com.github.erp.repository.Empresas;
+
+@Named
+@ViewScoped
+public class GestaoEmpresasBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Empresas empresas;
+	
+	private List<Empresa> listaEmpresas;
+
+	public void todasEmpresas() {
+		listaEmpresas = empresas.todas();
+	}
+		
+	public List<Empresa> getListaEmpresas() {
+		return listaEmpresas;
+	}
+	
+	
+	
+}
--- src/main/java/com/github/erp/model/Empresa.java
+++ src/main/java/com/github/erp/model/Empresa.java
@@ -1,6 +1,7 @@
 package com.github.erp.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -49,6 +50,21 @@ public class Empresa implements Serializable {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable=false, length=30)
 	private TipoEmpresa tipo;
+	
+	@Column(precision = 10, scale = 2)
+	private BigDecimal faturamento;
+
+	public BigDecimal getFaturamento() {
+		return faturamento;
+	}
+
+	public void setFaturamento(BigDecimal faturamento) {
+		this.faturamento = faturamento;
+	}
+
+	public TipoEmpresa getTipo() {
+		return tipo;
+	}
 	
 	public void setTipo(TipoEmpresa tipo) {
 		this.tipo = tipo;
